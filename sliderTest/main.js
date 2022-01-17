472367365f236a8845ac54cc4109e308617a5b52
duboo <duboo31@gmail.com>
2022-01-17 23:41:53 +0900

    Add slider code

--- sliderTest/main.js
+++ sliderTest/main.js
@@ -1,20 +1,20 @@
-var ul = document.querySelector("ul");
-var width = document.querySelector("li").offsetWidth;
-var totalWidth = width * 60;
+// var ul = document.querySelector("ul");
+// var width = document.querySelector("li").offsetWidth;
+// var totalWidth = width * 5;
 
-ul.style.width = totalWidth + "px"; // 이미지 크기만큼 width 증가
+// ul.style.width = totalWidth + "px"; // 이미지 크기만큼 width 증가
 
-var moveLeftAntimation = ul.animate([
-  { transform: 'translateX(0px)' }, 
-  { transform: 'translateX(0px)', offset: 0.9}, 
-  { transform: 'translateX(-100px)' }
-], { 
-  duration: 2000,
-}); 
+// var moveLeftAntimation = ul.animate([
+//   { transform: 'translateX(0px)' }, 
+//   { transform: 'translateX(0px)', offset: 0.75}, 
+//   { transform: `translateX(-${width}px)` }
+// ], { 
+//   duration: 2000,
+// }); 
 
 
-moveLeftAntimation.onfinish = function() {
-  var removeItem = ul.removeChild(ul.firstElementChild);
-  ul.appendChild(removeItem); 
-  moveLeftAntimation.play();  
-}
+// moveLeftAntimation.onfinish = function() {
+//   var removeItem = ul.removeChild(ul.firstElementChild);
+//   ul.appendChild(removeItem); 
+//   moveLeftAntimation.play();  
+// }
