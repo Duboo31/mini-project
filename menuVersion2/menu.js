21608d0898890498472b959427ca7f355e2c937a
duboo <duboo31@gmail.com>
2022-03-16 23:41:31 +0900

    menu version2

---
+++ menuVersion2/menu.js
@@ -0,0 +1,7 @@
+const menuWrap = document.querySelector('.menu-wrap');
+const menuInner = document.querySelector('.menu-inner');
+
+
+window.addEventListener('click', (e) => {
+  console.log(e.target);
+})
