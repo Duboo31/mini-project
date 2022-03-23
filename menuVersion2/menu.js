c0fe1954a1edffd2cc32a51be8763c6efdee09c9
duboo <duboo31@gmail.com>
2022-03-23 23:43:39 +0900

    menu version2 menu.js

--- menuVersion2/menu.js
+++ menuVersion2/menu.js
@@ -1,7 +1,31 @@
-const menuWrap = document.querySelector('.menu-wrap');
-const menuInner = document.querySelector('.menu-inner');
+// const menu = document.querySelector('.menu');
+// let curMenu;
 
+// // active 클레스 추가, curMenu 변경 함수
+// const activate = (elem) => {
+//   elem.classList.add('menu-active');
+//   curMenu = elem;
+// }
 
-window.addEventListener('click', (e) => {
+// // active 클레스 삭제 함수
+// const inactivate = (elem) => {
+//   elem.classList.remove('menu-active');
+// }
+
+// // 메뉴 클릭 이벤트 핸들러
+// const clickHandler = (event) => {
+//   if(curMenu) {
+//     inactivate(curMenu);
+//   }
+//   activate(event.target)
+// }
+
+// menu.addEventListener('click', clickHandler);
+
+const tabMenu = document.querySelector('.tab-menu');
+
+const tabClickHandler = (e) => {
   console.log(e.target);
-})
+}
+
+tabMenu.addEventListener('click', tabClickHandler);
